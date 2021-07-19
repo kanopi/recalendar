f4e2dc5f84ab96f166b09534a80639fcff434b00
Anne Bonham <anne.bonham@kanopistudios.com>
2021-07-19 16:55:25 -0700

    tw26835739 - fixes coding standards issues

--- src/Controller/RecalendarController.php
+++ src/Controller/RecalendarController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\recalendar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
@@ -10,15 +11,52 @@ use Drupal\taxonomy\Entity\Term;
 class RecalendarController extends ControllerBase {
 
   /**
+   * QueryFactory for entity queries.
+   *
+   * @var \Drupal\sfsd_recalendar\Plugin\Block\QueryFactory
+   */
+  private $entityQuery;
+
+  /**
+   * EntityManager..
+   *
+   * @var \Drupal\sfsd_recalendar\Plugin\Block\EntityManager
+   */
+  private $entityManager;
+
+  /**
+   * Taxonomy terms.
+   *
+   * @var \Drupal\sfsd_recalendar\Plugin\Block\Term
+   */
+  private $term;
+
+  /**
+   * RecalendarBlock constructor.
+   *
+   * @param \Drupal\sfsd_recalendar\Plugin\Block\QueryFactory $entityQuery
+   *   For EntityQuery.
+   * @param \Drupal\sfsd_recalendar\Plugin\Block\EntityManager $entityManager
+   *   For EntityManager.
+   * @param \Drupal\sfsd_recalendar\Plugin\Block\Term $typeTerm
+   *   For Term.
+   */
+  public function __construct(QueryFactory $entityQuery, EntityManager $entityManager, Term $typeTerm) {
+    $this->entityQuery = $entityQuery;
+    $this->entityManager = $entityManager;
+    $this->term = $typeTerm;
+  }
+
+  /**
    * Returns a simple page.
    *
    * @return array
    *   A simple renderable array.
    */
-  public function Events() {
+  public function events() {
 
     // Get options from Event Type taxonomy vocabulary.
-    $type_query = \Drupal::entityQuery('taxonomy_term');
+    $type_query = $this->entityQuery('taxonomy_term');
     $type_query->condition('vid', "rec_event_type");
     $type_tids = $type_query->execute();
 
@@ -27,11 +65,14 @@ class RecalendarController extends ControllerBase {
     foreach ($type_tids as $type_tid) {
 
       // Loads taxonomy term using tid.
-      $type_term = Term::load($type_tid);
+      $type_term = $this->load($type_tid);
 
-      // We ultimately need option value to equal term tid, but Drupal will convert any integer we provide as an option
-      // into a sequential integer starting with 0. So we make it a string by prefixing it with term, which we strip on
-      // page load using jQuery replace.
+      /*
+       * We need option value to equal term tid, but Drupal will convert any
+       * integer we provide as an option into a sequential integer starting with
+       * 0. So we make it a string by prefixing it with term, which we strip on
+       * page load using jQuery replace.
+       */
       $type_filter_key = 'term' . $type_tid;
 
       // Gets taxonomy term name.
@@ -41,14 +82,14 @@ class RecalendarController extends ControllerBase {
       $type_term_names[$type_filter_key] = $type_filter_name;
     }
     // Add All option to start of array.
-    array_unshift($type_term_names,"All Events");
+    array_unshift($type_term_names, "All Events");
 
     $element = [
       // Select list for js filtering of events by type.
       $form['event_type'] = [
         '#type' => 'select',
         '#prefix' => '<div class="grid-x grid-margin-x calendar-filters"><span class="filters-wrapper">',
-        '#multiple' => false,
+        '#multiple' => FALSE,
         '#options' => $type_term_names,
         '#attributes' => [
           'id' => 'event_type',
@@ -56,23 +97,23 @@ class RecalendarController extends ControllerBase {
             'cell',
             'small-12',
             'medium-4',
-          ]
+          ],
         ],
       ],
 
-      // Select list for js filtering of events by month and year. JS will provide the options.
+      // Select list for js filtering of events by month and year.
       $form['event_date'] = [
         '#type' => 'select',
-        '#multiple' => false,
+        '#multiple' => FALSE,
         '#attributes' => [
           'id' => 'event_date',
           'class' => [
             'cell',
             'small-12',
             'medium-4',
-          ]
+          ],
         ],
-        '#suffix' => '</span></div>'
+        '#suffix' => '</span></div>',
       ],
       $form['meeting_calendar'] = [
         '#markup' => '<div id="calendar">&nbsp;</div>',
--- src/Controller/RecalendarEndpointController.php
+++ src/Controller/RecalendarEndpointController.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Drupal\recalendar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Provides endpoints for events and meetings data (consumed by Fullcalendar events calendar).
+ * Provides endpoints for events and meetings data (consumed by calendar).
  */
 class RecalendarEndpointController extends ControllerBase {
 
@@ -13,9 +14,9 @@ class RecalendarEndpointController extends ControllerBase {
    * Returns a simple page from a static JSON file.
    *
    * @return string
-   *
+   *   Json data.
    */
-  public function EventsEndpoint() {
+  public function eventsEndpoint() {
 
     $data = file_get_contents('public://data/data_export_events.json');
     $response = new Response($data);
